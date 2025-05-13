4b1f1112ae40715deb222c6138e2492638d3afd9
Abdullah_The_Analyst <150716560+AbdullahTheAnalyst@users.noreply.github.com>
2025-05-13 08:59:16 +0500

    Update app.py

--- app.py
+++ app.py
@@ -5,28 +5,28 @@ import pandas as pd
 # Title
 st.title("House Price Prediction App with Linear Regression Model")
 
-# Model va ustunlar ro'yxatini yuklash
+# Model va Model Columns
 model, model_columns = joblib.load("House_Prediction_Model_LinearRegression.pkl")
 
-# Location ustunlarini ajratib olish
+# Location Columns
 location_columns = [col for col in model_columns if col not in ["total_sqft", "bath", "bhk"]]
 
-# Foydalanuvchidan ma'lumot olish
+# Inputs
 location = st.selectbox("Location", sorted(location_columns))
 sqft = st.number_input("Total Square Footage", min_value=1)
 bath = st.number_input("Number of Bathrooms", min_value=1)
 bhk = st.number_input("Number of Bedrooms", min_value=1)
 
-# Bo'sh dataframe: barcha model ustunlari bilan
+# Empty dataframe
 input_df = pd.DataFrame(data=[[0]*len(model_columns)], columns=model_columns)
 
-# Raqamli ustunlarni joylashtirish
+# Integer inputs
 input_df.at[0, "total_sqft"] = sqft
 input_df.at[0, "bath"] = bath
 input_df.at[0, "bhk"] = bhk
 
 input_df = pd.DataFrame(columns=model_columns)
-input_df.loc[0] = 0  # barcha ustunlarga 0
+input_df.loc[0] = 0 
 
 # model_columns ichida mavjud bo‘lgan ustunlar uchun qiymat kiriting
 if "bath" in input_df.columns:
